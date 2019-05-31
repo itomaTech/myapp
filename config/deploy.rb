c08b4f5dc3ea4b65af312a202d7d652dea3f9491
itomaTech <tomoyakadowaki.1211@gmail.com>
2019-05-31 19:35:54 +0900

    tweak deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, "git@github.com:itomaTech/myapp.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
-set :linked_files, %w{ config/credentials.yml.enc }
+set :linked_files, %w{config/master.key}
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/myapp.pem']
@@ -16,29 +16,30 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# after 'deploy:publishing', 'deploy:restart'
-# namespace :deploy do
-#   task :restart do
-#     invoke 'unicorn:stop'
-#     invoke 'unicorn:start'
-#   end
-# end
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:stop'
     invoke 'unicorn:start'
   end
-
-  desc 'upload credentials.yml.enc'
-  task :upload do
-    on roles(:app) do |host|
-      if test "[ ! -d #{shared_path}/config ]"
-        execute "mkdir -p #{shared_path}/config"
-      end
-      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
-    end
-  end
-  before :starting, 'deploy:upload'
-  after :finishing, 'deploy:cleanup'
 end
+
+# after 'deploy:publishing', 'deploy:restart'
+# namespace :deploy do
+#   task :restart do
+#     invoke 'unicorn:stop'
+#     invoke 'unicorn:start'
+#   end
+
+#   desc 'upload credentials.yml.enc'
+#   task :upload do
+#     on roles(:app) do |host|
+#       if test "[ ! -d #{shared_path}/config ]"
+#         execute "mkdir -p #{shared_path}/config"
+#       end
+#       upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
+#     end
+#   end
+#   before :starting, 'deploy:upload'
+#   after :finishing, 'deploy:cleanup'
+# end
