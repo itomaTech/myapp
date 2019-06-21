4c5d9b71e234460784feb247b16924168e92dc3f
itomaTech <tomoyakadowaki.1211@gmail.com>
2019-06-21 15:46:26 +0900

    add validates

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_many_attached :images
+  validates :body, presence: true, length: { maximum: 144 }
 
   def thumbnail input
     return self.images[input].variant(resize: '150x150').processed
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:twitter]
 
+  validates :name, presence: true, length: { maximum: 8 } 
   has_many :tweets
   has_one_attached :avatar
 end
